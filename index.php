2394e41f1024c84930d29f0070e88cc97ece8294
Avellaneda Alejandro <Aleavellaneda1@gmail.com>
2018-03-09 09:36:53 -0300

    se incluye enlase al icono de incubate

--- index.php
+++ index.php
@@ -554,7 +554,7 @@
    <center style="background-color: #737373;">
     <ul class="actions" style="padding-left: 0px;">
             <a href="https://ademi.org.ar/aulavirtual/"><img src="images/Aula_Pie.png" alt="" width="200"></a>
-            <a href=""><img src="images/Incubate_Pie.png" alt="" width="200"></a>
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLSfSxcyFVx-TG6yHdikgG77zRpIx-wLnnz_VHtqxag_BdhwUqQ/viewform?c=0&w=1"><img src="images/Incubate_Pie.png" alt="" width="200"></a>
             <a href="http://registro.ademi.org.ar/"><img src="images/Profesionales_Pie.png" alt="" width="200"></a>
             <a href="https://www.pymesmisiones.com.ar/"><img src="images/PymesMisiones_Pie.png" alt="" width="200"></a>
     </ul>   
