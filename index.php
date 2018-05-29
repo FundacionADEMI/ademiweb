df5aef111935ee40e5360f8f72d321367376ee9c
Alejandro Avellaneda <Aleavellaneda1@gmail.com>
2018-05-29 12:35:03 -0300

    cambios de FeedBurner mailing
    cambios de FeedBurner mailing

--- index.php
+++ index.php
@@ -564,13 +564,7 @@
 </center>
             <footer class="android-footer mdl-mega-footer">
 
-<form style="border:1px solid #ccc;padding:3px;text-align:center;" action="https://feedburner.google.com/fb/a/mailverify" method="post" target="popupwindow" onsubmit="window.open('https://feedburner.google.com/fb/a/mailverify?uri=fundacionademi', 'popupwindow', 'scrollbars=yes,width=550,height=520');return true">
-  <p>Ingresa tu correo electronico</p>
-  <p><input type="text" style="width:140px" name="email"/>
-  </p><input type="hidden" value="fundacionademi" name="uri"/>
-  <input type="hidden" name="loc" value="es_ES"/><input type="submit" value="Subscribe" />
-  <p>Delivered by <a href="https://feedburner.google.com" target="_blank">FeedBurner</a></p>
-</form>
+
 
 <!-- Textfield with Floating Label -->
 
@@ -578,8 +572,13 @@
   <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
     <input class="mdl-textfield__input" type="text" name="email">
     <input type="hidden" value="fundacionademi" name="uri"/>
+      <input type="hidden" name="loc" value="es_ES"/>
+      <!-- Accent-colored raised button with ripple -->
+
+     
     <label class="mdl-textfield__label" for="sample3">Ingresa tu correo electronico</label>
   </div>
+<button class="mdl-button mdl-js-button mdl-button--raised  mdl-js-ripple-effect mdl-color--accent" style="    color: #fff;" value="Subscribe" type="submit">suscribirse</button>
 </form>
 
 
